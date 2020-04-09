64970d5c9700f97f01824265b359c48ae307836b
Chavret <g.glorian@groupeonepoint.com>
2020-04-09 11:02:26 +0200

    Promise Part 1

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,3 @@
-import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
 import { Component, OnInit, ViewChild } from '@angular/core';
